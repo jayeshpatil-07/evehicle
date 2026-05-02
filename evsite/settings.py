b196da4f56e0661c188504b54b614839b12edb33
jayeshpatil-07 <jayeshpati67@gmail.com>
2026-05-02 17:08:44 +0530

    database config

--- evsite/settings.py
+++ evsite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,15 +75,13 @@ WSGI_APPLICATION = 'evsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ev_charging_db',
-        'USER': 'root',
-        'PASSWORD': '1234',   # your MySQL password
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
